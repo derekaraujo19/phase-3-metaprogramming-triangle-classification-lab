c13ecd2cab7ce852c65d60289a96b87519ba2ae0
Derek Araujo <derekaraujo19@gmail.com>
2022-04-14 13:28:19 -0400

    commit tree 5b3f362f1d65b7fc97c9d3551f2678f84991df7a

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,40 @@
 class Triangle
-  # write code here
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    sides = [@side1, @side2, @side3]
+
+    # ERROR SECTION
+    sides.each do |side|
+      if side <= 0
+        raise TriangleError
+      end
+    end
+    if (@side1 + @side2) <= @side3
+      raise TriangleError
+    elsif (@side2 + @side3) <= @side1
+      raise TriangleError
+    elsif (@side1 + @side3) <= @side2
+      raise TriangleError
+    end
+
+
+    if sides.uniq.length == 1
+      :equilateral
+    elsif sides.uniq.length == 2
+      :isosceles
+    elsif sides.uniq.length == 3
+      :scalene
+    end
+
+  end
+
+  class TriangleError < StandardError
+    # triangle error code
+  end
+
 end
